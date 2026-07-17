63740bbd84e9ca4664dbef2892f23e159b5d917f
LuQvia <info@luqvia.com>
2026-07-17 10:20:23 +0900

    2

--- ja/assets/js/contact-form-config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Luqevora contact form configuration.
- * Google Apps Script web app endpoint used by both Japanese and English forms.
- */
-window.LUQEVORA_CONTACT_FORM = Object.freeze({
-  endpoint: 'https://script.google.com/macros/s/AKfycbwVokdx3R0bdhUbGP8jBWHCAFrGSh061wu6ASGE67qx0f91qvQjlXonrXULmSbJc3fhLA/exec',
-  formVersion: '1.9.0'
-});
